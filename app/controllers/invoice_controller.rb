d6b6fb5d7e8658fb9df2ed8e02fd400b27b5e247
Matt Matero <matthewmatero@mail.adelphi.edu>
2016-06-12 18:36:29 -0400

    added post and permit params for create of invoice on backend

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -2,7 +2,19 @@ class InvoiceController < ApplicationController
 
   def index
   end
-  
+ 
+  #Should retrieve request from frontend to create a new param 
   def create
+    invoiceParams = params[:invoice]
+    puts invoiceParams.inspect
+
+    #@invoice = Invoice.new
+  end
+
+  def show
+  end
+
+  def invoice_params
+    params.require(:invoice).permit!
   end
 end
