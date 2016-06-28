67fe4eb4ec47dd336aa7cd28c9f2322ecdb039fd
Matt Matero <matthewmatero@mail.adelphi.edu>
2016-06-27 22:14:37 -0400

    CSRF workaround for post request. Temp solution

--- app/assets/javascripts/angular/services/invoiceService.js
+++ app/assets/javascripts/angular/services/invoiceService.js
@@ -4,16 +4,16 @@ invoiceService.$inject = ['$rootScope', '$http', '$resource'];
 function invoiceService($rootScope, $http, $resource){
   var service = this;
 
-  service.resource = $resource('invoices/new',
-    {id: 1, name: "Michelle"}, // default param
+  service.resource = $resource('new/',
+    {}, // default param
     {
-      createInvoice: { method: "POST", params: {} }
+      createInvoice: { method: "POST", params: {}}
     });
 
   service.createInvoice = function(){
     console.log("Sending data...");
 
-    service.resource.createInvoice();
+    service.resource.createInvoice({client: {name: 'matt'}});
   };
 
 }
--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -1,12 +1,16 @@
 class InvoiceController < ApplicationController
-
+  skip_before_filter  :verify_authenticity_token
   def index
   end
  
   #Should retrieve request from frontend to create a new param 
   def create
     invoiceParams = params[:invoice]
+    client = params[:client] unless invoiceParams.nil?
+    tasks = params[:tasks] unless invoiceParams.nil?
     puts invoiceParams.inspect
+    puts client.inspect
+    puts tasks.inspect
 
     #@invoice = Invoice.new
   end
